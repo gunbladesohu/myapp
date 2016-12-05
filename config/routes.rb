151be9840fa800b00729d395e67261d27f7d6e64
ChenMo <gunbladesohu@gmail.com>
2016-12-05 22:15:50 +0000

    add for activation email

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   resources :methodologies
   
   resources :users
-  
+  resources :account_activations, only: [:edit]  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'static_pages#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205043018) do
+ActiveRecord::Schema.define(version: 20161205093818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,11 +84,14 @@ ActiveRecord::Schema.define(version: 20161205043018) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
