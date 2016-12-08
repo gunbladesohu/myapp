c36b8aeca5216e4a2b0ede5e01b5489cca3d581f
ChenMo <gunbladesohu@gmail.com>
2016-12-08 10:04:38 +0000

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],git checkout -b user-microposts
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
