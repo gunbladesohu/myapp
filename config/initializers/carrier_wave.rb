088555c8e9c00c3393bd352b4a34003539473b77
ChenMo <gunbladesohu@gmail.com>
2016-12-08 11:20:07 +0000

    recover wave.rb

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Amazon S3
+      :region => "Oregen",
+      :provider              => "AWS",
+      :aws_access_key_id     => "11111111",
+      :aws_secret_access_key => "11111111"
+    }
+    config.fog_directory     =  "serler-ruby"
+  end
+end
