230c6c0a1626a8cf2a865208a3e192a225ad855c
ChenMo <gunbladesohu@gmail.com>
2016-12-08 12:47:31 +0000

    aaaa

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = { region: 'Oregen', provider: AWS , aws_access_key_id: 11111111 , aws_secret_access_key: 11111111
+    config.fog_credentials = { region: 'Oregen', provider: 'AWS' , aws_access_key_id: 11111111 , aws_secret_access_key: 11111111
     }
     config.fog_directory     =  "serler-ruby"
   end
