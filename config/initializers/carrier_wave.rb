6ff426a2dd7810d0308bc6898c965dcb0c56ac9f
ChenMo <gunbladesohu@gmail.com>
2016-12-08 11:00:47 +0000

    update carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,11 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :region => ENV['S3_REGION'],
+      :region => "Oregen",
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_access_key_id     => "11111111",
+      :aws_secret_access_key => "11111111"
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  "serler-ruby"
   end
 end
